9beee3bc86afbdd2a1c9991978ef0eed5d68a525
Julie Foucher <jumiloarking@gmail.com>
2021-06-10 01:42:27 +0200

    css page principale

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php if (isset($title)){echo $title;} else {echo 'GBAF';} ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     
 </head>
 <body>
