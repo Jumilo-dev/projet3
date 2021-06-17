4b0690a77ee8b6c562d5d50094e30e6c470c2dee
Julie Foucher <jumiloarking@gmail.com>
2021-06-17 16:10:51 +0200

    MAJ oubli MDP ok

--- new_pass.php
+++ new_pass.php
@@ -16,36 +16,41 @@ $title ="Mot de passe oublié";
         $user= $db->prepare ("SELECT * FROM `users` WHERE `id_user`= ? AND`reponse`=?");
         $user->execute(array($id_user,$inforep));
         $info_user=$user->fetch();
+        $id=$info_user['id_user'];
         
-        var_dump($info_user);
+        var_dump($id);
 
         if(!$info_user){
             header ("Location: index.php");
         }
         
-        /*Traitement enregistrement nouveau mot de pass 
-        if (isset($_POST[""],$_POST["reponse"])
-        && !empty($_POST["id_user"]) && !empty($_POST["reponse"]))
-        $newpass= password_hash($_POST["newpass"],PASSWORD_DEFAULT);
-        $username= strip_tags($_POST["username"]);
+         
+        /*if (isset($_POST["newpass"],$_POST["id"]) 
+        && !empty($_POST["newpass"]) && !empty($_POST["id"])){
 
-        $update_mdp= $db->prepare("UPDATE users SET `password`=? WHERE username=? LIMIT 1");
-        $update_mdp->execute(array($newpass,$username));
-        $count = $update_mdp->rowCount();
-                print('Mise à jour de ' .$count. ' entrée(s)');
-        echo "Modifier votre mot de passe";
+        $newpass= password_hash($_POST["newpass"],PASSWORD_DEFAULT);
+        $id=($_POST["id"]);
         
-    }*/
-    
+
+        $update_mdp= $db->prepare("UPDATE users SET `password`=? WHERE `id_user`= ? LIMIT 1");
+        $update_mdp->execute(array($newpass,$id));
+        $count = $update_mdp->rowCount();
+        print('Mise à jour de ' .$count. ' entrée(s)');
+        echo "Mot de passe modifier avec succès";
+        
+        }*/
+    }
 }
 ?>
 
 <body>
     <p>Création nouveau mot de passe</p>
-    <form  method="POST" >
-    <div class="form-group">
-        <label class=" control-label" for="newpass">Mot de passe </label> 
-        <input type="text" name="newpass" class= "form-control" required>
-    </div>
+    <form action ="traitement3.php" method="POST" >
+    <input type="hidden" name="id"  value="<?=$id?>" required>
+        <label for="newpass">Choisissez votre nouveau mot de passe </label> 
+        <input type="text" name="newpass"  required>
+        <button type="submit">Valider</button>
+ 
+    </form>
 </body>
  
---
+++ traitement3.php
@@ -0,0 +1,15 @@
+<?php 
+include 'includes/connect_bdd.php';
+if (isset($_POST["newpass"],$_POST["id"]) 
+        && !empty($_POST["newpass"]) && !empty($_POST["id"])){
+
+        $newpass= password_hash($_POST["newpass"],PASSWORD_DEFAULT);
+        $id=($_POST["id"]);
+        var_dump($id);
+
+        $update_mdp= $db->prepare("UPDATE users SET `password`=? WHERE `id_user`= ? LIMIT 1");
+        $update_mdp->execute(array($newpass,$id));
+        $count = $update_mdp->rowCount();
+        print('Mise à jour de ' .$count. ' entrée(s)');
+        echo "Mot de passe modifier avec succès";
+        }
